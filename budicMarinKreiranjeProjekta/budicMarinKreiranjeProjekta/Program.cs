9ffa09aa7c251074af0c434b085c646759372876
budicmarin <55134480+budicmarin@users.noreply.github.com>
2019-10-24 11:30:15 +0200

    Add files via upload

---
+++ budicMarinKreiranjeProjekta/budicMarinKreiranjeProjekta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinKreiranjeProjekta
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            sbyte sto = 100;
+            short tisu = -1000;
+            char slovo = 'a';
+            float dec = 3.123f;
+            float decim = 3.123456789f;
+            bool istina = true;
+            string rijec = "true";
+            Console.Write("sto= {0}, tisu={1}, slovo={2},",sto,tisu,slovo);
+            Console.Write("dec={0},decim={1},istina={2},rijec={3}",dec,decim,istina,rijec);
+            Console.WriteLine(" ");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
